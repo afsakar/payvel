b4f32419ae6560c8c6a6509b40457000b022b11c
Azad Furkan ŞAKAR <afsakarr@gmail.com>
2023-06-18 13:15:26 +0300

    Redirect index route to panel

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('filament.pages.dashboard');
 });
 
 Route::get('/dashboard', function () {
